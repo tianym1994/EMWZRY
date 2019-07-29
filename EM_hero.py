42222058579b37dce6a38c22d618b523c4f58717
tianym1994 <tianym1994@163.com>
2019-07-29 17:53:16 +0800

    王者荣耀的聚类代码

--- EM_hero.py
+++ EM_hero.py
@@ -21,7 +21,7 @@ sns.heatmap(corr,annot=True)
 plt.show()
 
 #相关性大的属性保留一个，因此可以对属性进行降维
-features_main =  [u'最大生命', u'生命成长', u'最大法力', u'法力成长', u'初始法力', u'最高物攻', u'物攻成长', u'初始物攻', u'最大物防', u'最大每5秒回血', u'每5秒回血成长', u'最大每5秒回蓝', u'每5秒回蓝成长', u'最大攻速', u'攻击范围']
+features_main =  [u'最大生命', u'生命成长', u'最大法力', u'法力成长', u'初始法力', u'最高物攻', u'物攻成长', u'初始物攻', u'最大物防', u'物防成长',u'最大每5秒回血', u'每5秒回血成长', u'初始每5秒回血', u'最大每5秒回蓝', u'每5秒回蓝成长', u'最大攻速', u'攻击范围']
 data=data_ori[features_main]
 data[u'最大攻速'] = data[u'最大攻速'].apply(lambda x: float(x.strip('%'))/100)
 data[u'攻击范围']=data[u'攻击范围'].map({'远程':1,'近战':0})
@@ -36,4 +36,4 @@ prediction=gmm.predict(data)
 print(prediction)
 #将结果输出到CSV文件
 data_ori.insert(0,'分组',prediction)
-data_ori.to_csv('./hero_out',index=False,sep=',')
+data_ori.to_csv('./hero_out.csv',index=False,sep=',')
